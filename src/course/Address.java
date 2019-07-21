9afcdc9f651cc598e68df4d8a70eb4a18b717a4c
kirkpreston <prestokp@rose-hulman.edu>
2019-07-21 09:24:58 -0500

    Initial commit- Establishing the project

---
+++ src/course/Address.java
@@ -0,0 +1,26 @@
+package course;
+
+public class Address {
+
+  private String streetAddress;
+  private String city;
+  private String state;
+  private long zipCode;
+
+  public Address(String streetAddress, String city, String state, long zipCode) {
+    this.streetAddress = streetAddress;
+    this.city = city;
+    this.state = state;
+    this.zipCode = zipCode;
+  }
+
+  @Override
+  public String toString() {
+    String result;
+
+    result = streetAddress + "\n";
+    result += city + ", " + state + " " + zipCode;
+
+    return result;
+  }
+}
---
+++ src/course/CourseDriver.java
@@ -0,0 +1,9 @@
+package course;
+
+public class CourseDriver {
+
+  public static void main(String[] args) {
+
+  }
+
+}
