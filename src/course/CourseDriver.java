f6711eb8d87fae0211bc84dfe2b09c33ccb36e36
prestokp <prestokp@rose-hulman.edu>
2019-07-24 10:06:46 -0500

    Updated the student class with overloaded constructors Updated the CourseDriver class with file processing and course class methods

--- src/course/CourseDriver.java
+++ src/course/CourseDriver.java
@@ -1,9 +1,50 @@
 package course;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class CourseDriver {
 
   public static void main(String[] args) {
 
-  }
+    Scanner sc = null; //assigned a value to prevent a program failure
+    try{
+      sc = new Scanner(new File("students.txt")); //streams the file through file object
+    }
+    catch(IOException e){
+      e.printStackTrace();
+    }//Catch Block Delimiter
 
-}
+    while(sc.hasNext()){
+
+      //Reads in the lines from the text file and assign
+      //them to string variables
+      String firstName = sc.nextLine();
+      String lastName = sc.nextLine();
+      String homeStreet = sc.nextLine();
+      String homeCity = sc.nextLine();
+      String homeState = sc.nextLine();
+      String homeZip = sc.nextLine();
+      String schoolStreet = sc.nextLine();
+      String schoolCity = sc.nextLine();
+      String schoolState = sc.nextLine();
+      String schoolZip = sc.nextLine();
+      String testScore1 = sc.nextLine();
+      String testScore2 = sc.nextLine();
+      String testScore3 = sc.nextLine();
+
+      //Burns the blank line so that the program can proceed with reading the file
+      if (sc.hasNext()){
+        sc.nextLine();
+      }//Conditional Delimiter
+
+      //Public methods from the course class to be implemented
+      course.roll();
+      course.average();
+
+    }//While loop delimiter
+
+  }//Main function delimiter
+
+}//Public Class Delimiter
--- src/course/Student.java
+++ src/course/Student.java
@@ -11,19 +11,26 @@ public class Student {
   private double score1, score2, score3;
 
   public Student(String firstName, String lastName, Address homeAddress, Address schoolAddress) {
-    this.firstName = firstName;
+    /*this.firstName = firstName;
     this.lastName = lastName;
     this.homeAddress = homeAddress;
-    this.schoolAddress = schoolAddress;
+    this.schoolAddress = schoolAddress;*/
+    this(firstName, lastName, homeAddress, schoolAddress, 0, 0, 0);
   }
 
   public Student(String firstName, String lastName, Address homeAddress, Address schoolAddress, double[] testScores){
-
+    this(firstName, lastName, homeAddress, schoolAddress, testScores[0], testScores[1], testScores[2]);
   }
 
   public Student(String firstName, String lastName, Address homeAddress, Address schoolAddress,
                  double score1, double score2, double score3){
-
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.homeAddress = homeAddress;
+    this.schoolAddress = schoolAddress;
+    testScores[0] = score1;
+    testScores[1] = score2;
+    testScores[2] = score3;
   }
 
   public double getTestScore(int testNumber){
@@ -51,6 +58,9 @@ public class Student {
     result = firstName + " " + lastName + "\n";
     result += "Home Address:\n" + homeAddress + "\n";
     result += "School Address:\n" + schoolAddress + "\n";
+    result += "Test Scores:\n" + testScores[0] + "\n" +
+            testScores[1] + "\n" + testScores[2] + "\n";
+    result += "Test Average:\n" + average(testScores);
 
     return result;
   }
