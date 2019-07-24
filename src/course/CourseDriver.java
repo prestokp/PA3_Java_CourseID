725942f4cda3b5902ea47bf63ed46e8a562de772
kirkpreston <prestokp@rose-hulman.edu>
2019-07-23 20:11:34 -0500

    Progress....

--- src/course/CourseDriver.java
+++ src/course/CourseDriver.java
@@ -1,8 +1,24 @@
 package course;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class CourseDriver {
 
   public static void main(String[] args) {
+
+    String[] students = new String[];
+
+    Scanner sc = null; //prevents program failure
+
+    try{
+      sc = new Scanner(new File("students.txt"));
+    }
+
+    catch(IOException e){
+      e.printStackTrace();
+    }
 
   }
 
--- src/course/Student.java
+++ src/course/Student.java
@@ -6,12 +6,34 @@ public class Student {
   private String lastName;
   private Address homeAddress;
   private Address schoolAddress;
+  private double[] testScores;
 
   public Student(String firstName, String lastName, Address homeAddress, Address schoolAddress) {
     this.firstName = firstName;
     this.lastName = lastName;
     this.homeAddress = homeAddress;
     this.schoolAddress = schoolAddress;
+  }
+
+  public Student(String firstName, String lastName, Address homeAddress, Address schoolAddress,
+                 double[] testScores){
+
+  }
+
+  public Student(String firstName, String lastName, Address homeAddress, Address schoolAddress,
+                 double testScores[0] = 0, double testScore){
+
+  }
+
+  public int setTestScore(int[] testScores, int testNumber){
+
+    return testNumber;
+  }
+
+  public double getTestScore(int testNumber){
+
+    return testScores[testNumber + 1];
+
   }
 
   @Override
